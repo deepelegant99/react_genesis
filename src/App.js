5a3537762e29cf19f058bd91a14773bb82a12ae0
Phillip Wong <deepelegant99@gmail.com>
2021-12-01 19:41:16 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [count, setCount] =useState(0)
+  
+  const countHandler = () =>{
+    setCount(count+1)
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <p>This is your count {count}</p>
+      <button onClick={countHandler}>By 1</button>
     </div>
   );
 }
