eaa8e6b11d01b0ae8fbfe23c40afd3e0e6b96447
Amir D. Sattari <adsattari@gmail.com>
2021-12-01 20:16:11 -0800

    Add material UI to project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Nav from './components/Nav';
+import Button from '@mui/material/Button';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const countHandler = () => {
-    setCount(count + 1);
-  };
+    const countHandler = () => {
+        setCount(count + 1);
+    };
 
-  return (
-    <div className="App">
-      <Nav />
-      <h1>Hello World</h1>
-      <p>This is your count {count}</p>
-      <button onClick={countHandler}>By 1</button>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Nav />
+            <h1>Hello World</h1>
+            <p>This is your count {count}</p>
+            <Button onClick={countHandler} variant='contained'>
+                By 1
+            </Button>
+        </div>
+    );
 }
 
 export default App;
