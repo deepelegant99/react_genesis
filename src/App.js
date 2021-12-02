1bbbe59fcb484a8566702acf4a2b5069620b08f0
phptuts <glaserpower@gmail.com>
2021-12-01 19:51:39 -0800

    created nav bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react';
+import Nav from './components/Nav';
 
 function App() {
-  const [count, setCount] =useState(0)
-  
-  const countHandler = () =>{
-    setCount(count+1)
-  } 
+  const [count, setCount] = useState(0);
+
+  const countHandler = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div className="App">
+      <Nav />
       <h1>Hello World</h1>
       <p>This is your count {count}</p>
       <button onClick={countHandler}>By 1</button>
